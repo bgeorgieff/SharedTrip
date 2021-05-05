878b9c50a8dfb92225c13252acdc73f393c096ed
bgeorgieff <b.g.georgiev87@gmail.com>
2021-05-05 17:33:28 +0300

    feat: added join trip and close trip

--- handlers/trips/index.js
+++ handlers/trips/index.js
@@ -14,20 +14,45 @@ module.exports = {
                 })
             })
         },
+
         offeredTrip(req, res, next) {
             res.render('./trips/offerTrip.hbs', {
                 isLoggedIn: req.user !== undefined,
                 userEmail: req.user ? req.user.email : ''
             }) 
         },
+        
         detailsTrip(req, res, next) {
             const {id} = req.params
             Trip.findById(id).lean().then((trip) => {
+                const currentUser = JSON.stringify(req.user._id)
+                const availableSeats = (trip.seats) - trip.otherPpl.length
+
                 res.render('./trips/detailsTrip.hbs', {
                     isLoggedIn: req.user !== undefined,
                     userEmail: req.user ? req.user.email : '',
-                    trip
+                    trip,
+                    isTheDriver: JSON.stringify(trip.driver) === currentUser,
+                    isAlreadyJoined: JSON.stringify(trip.otherPpl).includes(currentUser),
+                    areSeatsAvailable: availableSeats > 0,
+                    availableSeats
                 })
+            })
+        },
+
+        closeTrip(req, res, next) {
+            const {id} = req.params
+            Trip.deleteOne({_id: id}).then((r) => {
+                res.redirect('/trip/shared-trips')
+            })
+        },
+
+        joinTrip(req, res, next) {
+            const {id} = req.params
+            const { _id } = req.user
+
+            Trip.updateOne({_id: id}, {$push: {otherPpl: _id}}).then((upd) => {
+                res.redirect(`/trip/details-trip/${id}`)
             })
         }
     },
@@ -44,6 +69,17 @@ module.exports = {
             const [startPoint, endPoint] = directions.split(' - ')
             const [date, time] = dateTime.split(' - ')
             const { _id } = req.user
+
+            const errors = validationResult(req)
+
+            if(!errors.isEmpty()) {
+                    res.render('./trips/offerTrip.hbs', {
+                    isLoggedIn: req.user !== undefined,
+                    userEmail: req.user ? req.user.email : '',
+                    message: errors.array()[0].msg
+                })
+                return
+            }
 
             Trip.create({startPoint, endPoint, date, time, seats, carImage, description, driver: _id}).then((createdTrip) => {
                 res.redirect('/trip/shared-trips')
--- routes/trips.js
+++ routes/trips.js
@@ -1,12 +1,14 @@
 const router = require('express').Router()
 const handler = require('../handlers/trips')
 const isAuth = require('../utils/isAuth')
-const { playValidator } = require('../utils/validator')
+const validations = require('../utils/validator')
 
 router.get('/shared-trips', isAuth(), handler.get.sharedTrips)
 router.get('/offer-trips', isAuth(), handler.get.offeredTrip)
 router.get('/details-trip/:id', isAuth(), handler.get.detailsTrip)
+router.get('/close-trip/:id', isAuth(), handler.get.closeTrip)
+router.get('/join-trip/:id', isAuth(), handler.get.joinTrip)
 
-router.post('/offer-trips', isAuth(), handler.post.offeredTrip)
+router.post('/offer-trips', isAuth(), validations, handler.post.offeredTrip)
 
 module.exports = router
