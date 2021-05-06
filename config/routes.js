0062a8c822cfea67e852287ff60549dd7422e7e4
bgeorgieff <b.g.georgiev87@gmail.com>
2021-05-06 11:46:24 +0300

    feat: updated home link

--- config/routes.js
+++ config/routes.js
@@ -3,6 +3,8 @@ const router = require('../routes')
 module.exports = (app) => {
     app.use('/home', router.home)
 
+    app.use('/', router.home)
+
     app.use('/users', router.users)
 
     app.use('/trip', router.trips)
